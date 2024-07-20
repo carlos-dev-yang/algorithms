236977e29292ec3ba7abe51ffce822aa65ac0ce3
carlos-dev-yang <96582597+carlos-dev-yang@users.noreply.github.com>
2024-07-20 16:20:16 +0900

    feat: defeat 15

---
+++ leet/15-3sum.py
@@ -0,0 +1,47 @@
+from lib.leetcode_tester import run_tests
+from typing import List, Dict, Tuple, Optional
+from lib.ListNode import ListNode, create_linked_list, linked_list_to_string
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result_set = set()
+        nums.sort()
+        n = len(nums)
+        for i in range(n-2):
+            if nums[i] > 0:
+                break
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    result_set.add((nums[i], nums[left], nums[right]))
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return [list(triplet) for triplet in result_set]
+        
+          
+if __name__ == "__main__":
+    solution = Solution()
+    
+    test_cases = [
+        # 입력이 여러개이면 튜플로 묶기, 
+        # 출력이 여러개이면 리스트로 묶기
+        ([-1,0,1,2,-1,-4], [[-1,-1,2],[-1,0,1]]),
+        ([0,1,1], []),
+        ([0,0,0], [[0,0,0]]),
+    ]
+    
+    print("테스트 실행:")
+    # copyFromLeetcodeDefLine
+    run_tests(solution.threeSum, test_cases)
