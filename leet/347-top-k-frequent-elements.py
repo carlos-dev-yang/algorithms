c99fc8ccaed6aa57d5f500f26f092cf3370c08f6
carlos-dev-yang <96582597+carlos-dev-yang@users.noreply.github.com>
2024-08-01 22:50:24 +0900

    feat: defeat 347

--- leet/347-top-k-frequent-elements.py
+++ leet/347-top-k-frequent-elements.py
@@ -1,11 +1,14 @@
 from lib.leetcode_tester import run_tests
 from typing import List, Dict, Tuple, Optional
 from lib.ListNode import ListNode, create_linked_list, linked_list_to_string
+from collections import Counter
+import heapq
 
 class Solution:
     # copyFromLeetcodeDefLine
-    def copyFromLeetcodeDefLine(self, s: str) -> int:
-        print('type your code')
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
           
 if __name__ == "__main__":
     solution = Solution()
@@ -13,9 +16,9 @@ if __name__ == "__main__":
     test_cases = [
         # 입력이 여러개이면 튜플로 묶기, 
         # 출력이 여러개이면 리스트로 묶기
-        ("QUEST", 'ANSWER'),
+        (([1,1,1,2,2,3], 2), [1,2]),
+        (([1], 1), [1]),
     ]
     
     print("테스트 실행:")
-    # change method of solution
-    run_tests(solution.copyFromLeetcodeDefLine, test_cases)
+    run_tests(solution.topKFrequent, test_cases)
